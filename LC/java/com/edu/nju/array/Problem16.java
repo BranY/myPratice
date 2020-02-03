545c169c17f42f63286cb96fb5df14fda14b1743
BranY <yangwen_jia@163.com>
2020-02-03 22:55:53 +0800

    first add lc

---
+++ LC/java/com/edu/nju/array/Problem16.java
@@ -0,0 +1,34 @@
+package com.edu.nju.array;
+
+import java.util.Arrays;
+
+/**
+ * Description link:
+ * Tag:
+ * 2020/2/3_22:49  BranY
+ */
+public class Problem16 {
+    // https://leetcode-cn.com/problems/3sum-closest/solution/hua-jie-suan-fa-16-zui-jie-jin-de-san-shu-zhi-he-b/
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = nums[0] + nums[1] + nums[2];
+        for(int i=0;i<nums.length;i++) {
+            int start = i+1, end = nums.length - 1;
+            while(start < end) {
+                int sum = nums[start] + nums[end] + nums[i];
+                if(Math.abs(target - sum) < Math.abs(target - ans))
+                    ans = sum;
+                if(sum > target)
+                    end--;
+                else if(sum < target)
+                    start++;
+                else
+                    return ans;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
