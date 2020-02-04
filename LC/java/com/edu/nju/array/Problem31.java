5c57e6b638d6ba2bb749b4278dbd2c98c1ee905c
BranY <yangwen_jia@163.com>
2020-02-04 22:44:47 +0800

    first add lc

---
+++ LC/java/com/edu/nju/array/Problem31.java
@@ -0,0 +1,44 @@
+package com.edu.nju.array;
+
+/**
+ * Description link: https://leetcode-cn.com/problems/next-permutation/
+ * Tag:
+ * 2020/2/4_21:55  BranY
+ */
+public class Problem31 {
+    // leetcode-cn.com/problems/next-permutation/solution/xia-yi-ge-pai-lie-by-leetcode/
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
