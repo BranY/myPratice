53a01db0f3f4d6651574f59eb6530b4925c03139
BranY <yangwen_jia@163.com>
2020-04-28 00:29:47 +0800

    add cmakelist

---
+++ LC/cpp/main.cpp
@@ -0,0 +1,61 @@
+//
+// Created by 杨文家 on 2020/4/27.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void swap(vector<int>& nums, unsigned long i, unsigned long j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+}
+
+void reverse(vector<int>& nums, unsigned long start, unsigned long size) {
+    unsigned long i = start, j = size - 1;
+    while (i < j) {
+        swap(nums, i, j);
+        i++;
+        j--;
+    }
+}
+
+
+void nextPermutation(vector<int>& nums) {
+    unsigned long size = nums.size();
+    if (size <= 1) {
+        return;
+    }
+    auto i = size - 2, j = size - 1, k = size - 1;
+    while (i >=0 && nums[i] >= nums[j]) {
+        i--;
+        j--;
+    }
+
+    if (i >= 0) {
+        while(nums[i] >= nums[k]) k--;
+        swap(nums, i, k);
+    }
+
+    reverse(nums, j, size);
+}
+
+
+void test31() {
+    vector<int> A = {1,2,3,4,5,6};
+    nextPermutation(A);
+
+    cout << "cbengin:";
+
+    vector<int>::iterator it;
+    for (it=A.begin(); it!=A.end(); ++it)
+        cout << " " << *it;
+
+    cout << endl;
+}
+int main(int argc, char * argv[]) {
+   test31();
+    return 0;
+}
---
+++ LC/cpp/src/array/profile31.cpp
@@ -0,0 +1,44 @@
+//
+// Created by 杨文家 on 2020/4/27.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution31 {
+public:
+    void nextPermutation(vector<int>& nums) {
+        auto size = nums.size();
+        if (size <= 1) {
+            return;
+        }
+        auto i = size - 2, j = size - 1, k = size - 1;
+        while (i >=0 && nums[i] >= nums[j]) {
+            i--;
+            j--;
+        }
+
+        if (i >= 0) {
+            while(nums[i] >= nums[k]) k--;
+            swap(nums, i, k);
+        }
+
+        reverse(nums, j, size);
+    }
+    void reverse(vector<int>& nums, int start, int size) {
+        auto i = start, j = size - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    void swap(vector<int>& nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = nums[i];
+    }
+};
